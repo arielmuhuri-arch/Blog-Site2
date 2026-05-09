aaa96128e72e6057af62bcdce4299f15b1941e3f
Ariel Muhuri <ariel.muhuri@student.moringaschool.comgit config --global user.email ariel.muhuri@student.moringaschool.com>
2026-05-09 14:05:47 +0300

    My Blog Site

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Header from "./components/Header";
+import About from "./components/About";
+import ArticleList from "./components/ArticleList";
+import blog from "./blog";
+
+function App() {
+  return (
+    <div>
+      <Header name={blog.name} />
+
+      <About
+        image={blog.image}
+        about={blog.about}
+      />
+
+      <ArticleList posts={blog.posts} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/blog.js
@@ -0,0 +1,32 @@
+const blog = {
+  name: "Ariel's Tech Blog",
+  image:
+    "https://images.unsplash.com/photo-1516321318423-f06f85e504b3",
+  about:
+    "Sharing coding tips, React projects, and software development experiences.",
+  posts: [
+    {
+      id: 1,
+      title: "Getting Started with React",
+      date: "May 9, 2026",
+      preview:
+        "React makes it easy to build user interfaces using reusable components."
+    },
+    {
+      id: 2,
+      title: "Why Props Matter",
+      date: "May 10, 2026",
+      preview:
+        "Props help components communicate and make your app dynamic."
+    },
+    {
+      id: 3,
+      title: "Understanding JSX",
+      date: "May 11, 2026",
+      preview:
+        "JSX allows you to write HTML-like syntax directly inside JavaScript."
+    }
+  ]
+};
+
+export default blog;
